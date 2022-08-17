9c4770c6380974a148667c406431a90d4f728894
MAllport <mmallport@gmail.com>
2022-08-17 22:37:39 +0200

    Added basic model structure

--- recipe_django/app/models.py
+++ recipe_django/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class IngredientInfo(models.Model):
+    name = models.CharField(max_length = 100)
+
+class Ingredient(models.Model):
+    ingredient_info = models.ForeignKey(to = IngredientInfo, on_delete=models.CASCADE)
+    amount = models.CharField(max_length = 100, blank=True, default='')
+
+class Recipe(models.Model):
+    ingredient = models.ManyToManyField(to = Ingredient)
+    instructions = models.TextField(blank=True, default = '')
+    
