09054f9ba8b70c42b71c8bb21a2fb178c827e1f0
MAllport <mmallport@gmail.com>
2022-08-17 23:45:21 +0200

    Added nutritional values to model and added admin setup

--- recipe_django/app/models.py
+++ recipe_django/app/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
+class IngredientNutr(models.Model):
+    name = models.CharField(max_length=255)
+    unit = models.CharField(max_length=255)
+
 class IngredientInfo(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length=255)
+    ingredient_nutr = models.ManyToManyField(IngredientNutr, on_delete=models.CASCADE, blank=True, null=True)
 
 class Ingredient(models.Model):
-    ingredient_info = models.ForeignKey(to = IngredientInfo, on_delete=models.CASCADE)
-    amount = models.CharField(max_length = 100, blank=True, default='')
+    ingredient_info = models.ForeignKey(to= IngredientInfo, on_delete=models.CASCADE)
+    amount = models.CharField(max_length=255, blank=True, default='')
 
 class Recipe(models.Model):
-    ingredient = models.ManyToManyField(to = Ingredient)
-    instructions = models.TextField(blank=True, default = '')
-    
+    ingredient = models.ManyToManyField(to=Ingredient)
+    instructions = models.TextField(blank=True, default='')
